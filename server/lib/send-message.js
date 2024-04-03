e56c79170af65bd72433dc6528f4c8f50d1997c3
Tom Wilson <tom@hyper.io>
2024-04-03 20:30:13 +0000

    feat: update vouch pid default

--- server/lib/send-message.js
+++ server/lib/send-message.js
@@ -1,7 +1,7 @@
 import { createDataItemSigner, message, result } from '@permaweb/aoconnect'
 
 export async function sendMessage({ address }) {
-  const processId = process.env.VOUCH_DAO_PROCESS_ID || 'Aqc4_a5ZKAWLWQHlNoYB54DCaFIOHl9S-pI3HgdOLAs'
+  const processId = process.env.VOUCH_DAO_PROCESS_ID || '8qh1lX8dL-PjHZilwCy4kgwR7644IC6Z_gPfia-ij4E'
   const messageId = await message({
     process: processId,
     tags: [
