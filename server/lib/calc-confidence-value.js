a00e9bf2797de16bdf6bc9a74fd155f2d5fe03dc
Tom Wilson <tom@hyper.io>
2024-05-22 21:29:09 +0000

    update

--- server/lib/calc-confidence-value.js
+++ server/lib/calc-confidence-value.js
@@ -1,16 +1,17 @@
 // constants
 const TWEET_WEIGHT = 0.001
-const FOLLOWER_WEIGHT = 0.02
+const FOLLOWER_WEIGHT = 0.01
 const VERIFIED_WEIGHT = 8
 const LISTED_WEIGHT = 0.05
+const LIKE_WEIGHT = 0.01
 
 export function calculate(user) {
   // calculateConfidenceValue.js
-  const { followers_count, tweet_count, listed_count } = user.public_metrics;
+  const { followers_count, tweet_count, listed_count, like_count } = user.public_metrics;
   const verified = user.verified;
 
   const confidenceValue = Math.floor((tweet_count * TWEET_WEIGHT) +
-    (followers_count * FOLLOWER_WEIGHT) +
+    (like_count * LIKE_WEIGHT) +
     (listed_count * LISTED_WEIGHT) +
     (verified ? VERIFIED_WEIGHT : 0));
 
